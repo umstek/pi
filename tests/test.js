7d453625f2bfb76a4f113862af13a6d33ae0a9de
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-03-26 11:33:21 +0530

    Basic test/CI config

---
+++ tests/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chakram = require('chakram');
+var app = require('../server/server');
+
+describe('Chakram', function() {
+  it('should offer simple HTTP request capabilities', function() {
+    return chakram.get('http://httpbin.org/get');
+  });
+});
